0c1200a216cecba2d87126f74967817faa9b9cc5
Guma Master <analista.guma@gmail.com>
2022-08-23 09:33:27 -0600

    Fix: Validacion de Sessiones

--- public/sql-query.php
+++ public/sql-query.php
@@ -1,5 +1,8 @@
 <?php
-    session_start();
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
     function object_sorter($a,$b) {
         return (($a['PIN'] < $b['PIN']) ? 1 : -1);
     }
